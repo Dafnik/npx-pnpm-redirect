3ec91fc6d0142dee5f642b9346f5d7b5f2df6870
Dafnik <git@dafnik.me>
2026-04-30 23:36:41 +0200

    fix: github path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://dafnik.github.io",
+  base: "/npx-pnpm-redirect",
   integrations: [preact()],
   vite: {
     plugins: [tailwindcss()],
